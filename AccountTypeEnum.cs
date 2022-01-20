fbca41c5a41f619c1f307fa1a32c19eb3f5142d0
TemporarYLife <aay23@tpu.ru>
2022-01-20 16:03:42 +0700

    Done HW3 And HW 2

---
+++ AccountTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anatoly.Finanse
+{
+    internal enum AccountTypes
+    {
+        Payment=0,
+        Budget,
+        Savings,
+        Checking,
+        ForeignCurrency
+
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anatoly.Finanse
+{
+    class Staff
+    {
+        //HW3 Task 2 (Перевернуть строку)
+        public static string ReverseString(string initialString)
+        {
+            char[] stringToCharArray = initialString.ToCharArray();
+            return new string(stringToCharArray.Reverse().ToArray());
+        }
+
+        public static void AllocateMail(string initFilePath, string newFilePath)
+        {
+            if (File.Exists(initFilePath))
+            {
+                string fileText = File.ReadAllText(initFilePath);
+                string[] wordsArray = fileText.Split(" ");
+
+                if (File.Exists(newFilePath))
+                {
+                    File.Delete(newFilePath);
+                }
+
+                foreach (string word in wordsArray)
+                {
+                    if (word.Contains("@"))
+                    {
+                        File.AppendAllText(newFilePath, $"{word}\n");
+                    }
+                }
+                
+            }
+        }
+
+        
+    }
+}
