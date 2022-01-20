da81fb6b9a3f9aab13430ba6dc2901f8ed90cd9f
TemporarYLife <aay23@tpu.ru>
2022-01-20 16:05:10 +0700

    Task 3 left

---
+++ BuildingDescription.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anatoly.Buildings
+{
+    class BuildingDescription
+    {
+        private int _id;
+        private int _height;
+        private int _flatsCount;
+        private byte _floorsCount;
+        private byte _porchCount;
+
+        private static int _idCounter;
+
+        private BuildingDescription() : this(0)
+        {
+        }
+
+
+        private BuildingDescription(int height) : this(height, 0)
+        {
+        }
+
+        private BuildingDescription(int height, int flatsCount) : this(height, flatsCount, 0)
+        {
+        }
+
+        private BuildingDescription(
+            int height,
+            int flatsCount,
+            byte floorsCount) : this(height, flatsCount, floorsCount, 0)
+        {
+        }
+
+        private BuildingDescription(
+            int height,
+            int flatsCount,
+            byte floorsCount,
+            byte porchCount)
+        {
+            IncrementID();
+            _id = _idCounter;
+            _height = height;
+            _flatsCount = flatsCount;
+            _floorsCount = floorsCount;
+            _porchCount = porchCount;
+
+        }
+
+        public void SetAllFields(int height, int flatsCount, byte floorsCount, byte porchCount)
+        {
+            _height = height;
+            _flatsCount = flatsCount;
+            _floorsCount = floorsCount;
+            _porchCount = porchCount;
+        }
+
+        public void ShowAllFields()
+        {
+            Console.WriteLine($"ID: {_id}\nHeight: {_height}\nCount of flats: {_flatsCount}\n" +
+                $"Count of floors: {_floorsCount}\nCount of porchs: {_porchCount} ");
+        }
+
+        public int GetID() => _id;
+
+        public int GetFloorHeight() => (_floorsCount == 0) ? -1 : _height / _floorsCount;
+
+        public int FlatsInPorch() => (_porchCount == 0) ? -1 : _flatsCount / _porchCount;
+
+        public int FlatsOnFloor() => (_floorsCount==0)? -1: FlatsInPorch()/_floorsCount;
+
+        private void IncrementID() => _idCounter++;
+
+
+
+
+        class Creator
+        {
+            private static Hashtable _buildings = new Hashtable();
+
+            private Creator()
+            {
+
+            }
+
+            public static BuildingDescription CreateBuilding(int height, int flatsCount, byte floorsCount, byte porchCount)
+            {
+                BuildingDescription newBuilding = new BuildingDescription(height, flatsCount, floorsCount, porchCount);
+                _buildings.Add(newBuilding.GetID(), newBuilding);
+                return newBuilding;
+            }
+
+            public static void DeleteBuilding(int id) => _buildings.Remove(id);
+
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 
-namespace Anatoly.Finanse
+
+namespace Anatoly.Buildings
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
-        {/*
-            BankAccount Sender = new BankAccount(500, AccountTypes.Payment);
-            BankAccount Receiver = new BankAccount(0, AccountTypes.Payment);
-            Console.WriteLine(Sender.AccountBalance);
-            Console.WriteLine(Receiver.AccountBalance);
-            Sender.SendMoney(Receiver, 500);
-            Console.WriteLine(Sender.AccountBalance);
-            Console.WriteLine(Receiver.AccountBalance);*/
-
-            Staff.AllocateMail(@"D:\Documents\Desktop\test.txt", @"D:\Documents\Desktop\new.txt");
+        {
+            
         }
     }
 }
